38a87c2b21209d9b5e5261c92e660994dd2794b5
maxsroka <36799862+maxsroka@users.noreply.github.com>
2025-10-26 15:04:32 +0100

    Update RoundTimer.cs

--- Code/Round/RoundTimer.cs
+++ Code/Round/RoundTimer.cs
@@ -7,6 +7,12 @@ public sealed class RoundTimer : Component
 	[Sync(SyncFlags.FromHost)]
 	public RoundStage Stage { get; private set; } = RoundStage.Waiting;
 	
+	[ConVar("min_players")]
+	static int MinPlayers { get; set; } = 2;
+
+	[ConVar("wait_time")]
+	static float WaitTime { get; set; } = 30f;
+
 	[ConVar("prep_time")]
 	static float PrepTime { get; set; } = 5f;
 
@@ -19,48 +25,84 @@ public sealed class RoundTimer : Component
 	[Property, ReadOnly]
 	float timer = 0f;
 
+	protected override void OnStart()
+	{
+		base.OnStart();
+	}
+
 	protected override void OnUpdate()
 	{
 		if (!Connection.Local.IsHost) return;
-		if (Stage == RoundStage.Waiting) return;
 
 		timer += Time.Delta;
 
-		if (Stage == RoundStage.Preparing)
+		if (Stage == RoundStage.Waiting)
+        {
+			if (timer >= WaitTime)
+			{
+				Prepare();
+			}
+
+			if (PlayerFinder.All.Count() < MinPlayers)
+			{
+				timer = 0f;
+            }
+        }
+		else if (Stage == RoundStage.Preparing)
 		{
 			if (timer >= PrepTime)
 			{
-				Stage = RoundStage.Playing;
-				timer = 0f;
-				IRoundEvent.Post(e => e.OnPlaying());
+				Play();
 			}
 		}
 		else if (Stage == RoundStage.Playing)
 		{
 			if (timer >= PlayTime)
 			{
-				Stage = RoundStage.Ending;
-				timer = 0f;
-				IRoundEvent.Post(e => e.OnEnding());
+				End();
 			}
 		}
 		else if (Stage == RoundStage.Ending)
 		{
 			if (timer >= EndTime)
 			{
-				Stage = RoundStage.Waiting;
-				timer = 0f;
-				IRoundEvent.Post(e => e.OnWaiting());
+				Prepare();
 			}
 		}
 	}
 
+	void Wait()
+    {
+		Stage = RoundStage.Waiting;
+		timer = 0f;
+		IRoundEvent.Post(e => e.OnWaiting());
+    }
+
+	void Prepare()
+	{
+		Stage = RoundStage.Preparing;
+		timer = 0f;
+		IRoundEvent.Post(e => e.OnPreparing());
+	}
+
+	void Play()
+    {
+		Stage = RoundStage.Playing;
+		timer = 0f;
+		IRoundEvent.Post(e => e.OnPlaying());
+    }
+	
+	void End()
+    {
+		Stage = RoundStage.Ending;
+		timer = 0f;
+		IRoundEvent.Post(e => e.OnEnding());
+    }
+
 	[ConCmd("start", ConVarFlags.Server)]
 	static void Start()
 	{
-        var roundTimer = Game.ActiveScene.Get<RoundTimer>();
-		roundTimer.Stage = RoundStage.Preparing;
-		roundTimer.timer = 0f;
-		IRoundEvent.Post(e => e.OnPreparing());
+		var roundTimer = Game.ActiveScene.Get<RoundTimer>();
+		roundTimer.Prepare();
 	}
 }
