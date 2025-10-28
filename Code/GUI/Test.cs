6cf17f74126bfe39d1f0e4af2ecfece5aa6d1f46
maxsroka <36799862+maxsroka@users.noreply.github.com>
2025-10-28 19:42:29 +0100

    chatbox improvements

---
+++ Code/GUI/Test.cs
@@ -0,0 +1,18 @@
+using Sandbox.UI;
+
+class X : Panel
+{
+    public override void Tick()
+    {
+        base.Tick();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="e"></param>
+    protected override void OnEscape(PanelEvent e)
+    {
+        base.OnEscape(e);
+    }
+}
