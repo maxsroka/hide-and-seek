9657712c9422ff96e5b500e95fe306cc9512fc28
maxsroka <36799862+maxsroka@users.noreply.github.com>
2025-11-22 17:17:20 +0100

    Update Chat.cs

--- Code/GUI/Chat/Chat.cs
+++ Code/GUI/Chat/Chat.cs
@@ -12,12 +12,16 @@ public static class Chat
     [Rpc.Broadcast]
     public static void UserMessage(string message)
     {
+		#if !SERVER
 		IMessageListener.Post(e => e.OnUserMessage(message, Rpc.Caller));
-    }
+		#endif
+	}
 
     [Rpc.Broadcast(NetFlags.HostOnly)]
     public static void SystemMessage(string message)
     {
+		#if !SERVER
 		IMessageListener.Post(e => e.OnSystemMessage(message));
+		#endif
 	}
 }
