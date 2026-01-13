b233dbd79c76c64831349eb7454bd27408f666ee
maxsroka <36799862+maxsroka@users.noreply.github.com>
2026-01-13 14:05:19 +0100

    Update Preparing.cs

--- Code/Round/Preparing.cs
+++ Code/Round/Preparing.cs
@@ -3,7 +3,7 @@ namespace HNS;
 
 public class Preparing : Stage
 {
-	[ConVar("prep_time", ConVarFlags.GameSetting, Help = "Set the preparation time for hiding.")]
+	[ConVar("prep_time", ConVarFlags.GameSetting, Help = "Set the preparation time before a round begins.")]
 	[Range(0, 30)]
 	public static int PrepTime { get; set; } = 10;
 	public override float Duration => PrepTime;
